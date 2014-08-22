20762f2f4279b3a26a0e5b5b77db52478d0e239f
iissnan <iissnan@gmail.com>
2014-08-22 17:05:31 +0800

    Using bootstrap instead of angular-strap

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,5 @@ mainModule
         $locationProvider.hashPrefix('!');
     })
     .run(function ($rootScope) {
-        $rootScope.navigation = {
-            contents: [],
-            languages: [
-                { text: 'English', href: '#' },
-                { text: '简体中文', href: '#' },
-                { text: '正體中文', href: '#' }
-            ],
-            about: {}
-        } ;
+
     });

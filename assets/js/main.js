0834485e61b90bddd94ac07ad40a038935735aeb
iissnan <iissnan@gmail.com>
2014-08-26 22:18:36 +0800

    It works. Alpha version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,42 @@
-var mainModule = angular.module('progit', ['ngAnimate', 'mgcrea.ngStrap', 'ngProgress']);
+var mainModule = angular.module('progit', [
+    'ngRoute',
+    'ngAnimate',
+    'ngSanitize',
+    'ngProgress'
+]);
 
 mainModule
-    .config(function($locationProvider) {
+    .config(function($locationProvider, $routeProvider) {
         $locationProvider.hashPrefix('!');
-    })
-    .run(function ($rootScope) {
 
+        $routeProvider
+            .when('/', {
+                templateUrl: function (params) {
+                    var lang = params.lang || 'zh';
+                    return 'html/' + lang + '/index.html';
+                }
+            })
+            .when('/html/:lang/:chapter', {
+                templateUrl: function (params) {
+                    return 'html/' + params.lang + '/' + params.chapter;
+                }
+            })
+            .when('/about', {
+                templateUrl: 'html/about.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            })
+    })
+    .run(function ($rootScope, ngProgress) {
+        $rootScope.$on('$routeChangeStart', function () {
+            ngProgress.color('#00aeef');
+            ngProgress.height('3px');
+            ngProgress.reset();
+            ngProgress.start();
+        });
+
+        $rootScope.$on('$routeChangeSuccess', function () {
+            ngProgress.complete();
+        });
     });
